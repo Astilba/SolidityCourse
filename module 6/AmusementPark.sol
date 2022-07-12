dca0c0d77bdf6bafb9d90c0b1ac70ec0d59c134c
Babich Mariia <astilba22@gmail.com>
2022-07-13 02:02:37 +0300

    finish_module6

--- module 6/AmusementPark.sol
+++ module 6/AmusementPark.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.15;
+pragma solidity 0.8.15;
 
-import "./Attraction.sol";
-import "./Food.sol";
 import "./DisneyToken.sol";
 import "./IDisneyPark.sol";
 
@@ -12,79 +10,171 @@ error AmusementPark__TheAttractionAlreadyExists();
 error AmusementPark__TheAttractionDoesNotExist();
 error AmusementPark__TheFoodAlreadyExists();
 error AmusementPark__TheFoodDoesNotExist();
+error AmusementPark__TheAttractionIsNotAvailable();
+error AmusementPark__TheFoodIsNotAvailable();
+error AmusementPark__NotEnoughEther();
+error AmusementPark__NotEnoughTokensOnTheParkBalance();
+error AmusementPark__NotEnoughTokensOnTheClientBalance();
+error AmusementPark__AttractionStatusIsAlreadySetted();
+error AmusementPark__FoodStatusIsAlreadySetted();
+error AmusementPark__NumTokensIsIncorrect();
 
-contract AmusementPark is IDisneyPark {
+contract AmusementPark is IDisneyPark, Ownable {
+
     DisneyToken immutable i_token;
-    address public s_owner;
     string[] private s_attractionNames;
-    mapping(string => address) private s_attractions;
     string[] private s_foodNames;
-    mapping(string => address) private s_food;
+    mapping(string => Attraction) private s_attractions;
+    mapping(string => Food) private s_food;
+    mapping(address => Client) public clients;
+    mapping(address => string[]) private s_historyAttractions;
+    mapping(address => string[]) private s_historyFood;
+    mapping(PassType => PassSettings) public s_passSettings;
 
-    event OwnerSetted(address indexed _owner);
-    event AttractionAdded(string indexed _name, address indexed _attraction);
-    event FoodAdded(string indexed _name, address indexed _food);
-
-    constructor(address _token) {
-        i_token = DisneyToken(_token);
-        s_owner = msg.sender;
-        emit OwnerSetted(s_owner);
+    constructor() {
+        i_token = new DisneyToken(_msgSender(), address(this), 10000 * 10**2);
     }
 
-    modifier onlyOwner {
-        require(msg.sender == s_owner, "You do not have permission to run this function. Only owner allowed.");
-        _;
+    function tokenPrice(uint256 _numTokens) internal view returns(uint256) {
+        // Конвертация токенов в эфиры: 1 токен -> 0.1 эфира
+        return (_numTokens / (10**token.decimals())) * (0.01 ether);
     }
 
-    function setOwner(address _newOwner) external onlyOwner {
-        if (_newOwner == address(0)) {
-            revert AmusementPark__NewOwnerAddressIsIncorrect();
+    function buyTokens(uint256 _numTokens) external payable {
+        uint256 price = tokenPrice(_numTokens);
+        if (msg.value < price) {
+            revert AmusementPark__NotEnoughEther();
         }
-        s_owner = _newOwner;
-        emit OwnerSetted(_newOwner);
+        uint256 balance = balanceOf();
+        if (balance < _numTokens) {
+            revert AmusementPark__NotEnoughTokensOnTheParkBalance();
+        }
+        i_token.transfer(_msgSender(), _numTokens);
+        uint256 change = msg.value - price;
+        payable(_msgSender()).transfer(change);
+        clients[_msgSender()].tokensBuyed += _numTokens;
+    }
+
+    function balanceOf() external view returns(uint256) {
+        return i_token.balanceOf(address(this));
+    }
+
+    function myTokens() external view returns(uint256) {
+        return i_token.balanceOf(_msgSender());
+    }
+
+    function generateNewTokens(uint256 _numTokens) external onlyOwner {
+        i_token.mint(address(this), _numTokens);
     }
 
     function addAttraction(string memory _name, uint256 _price, bool _status) external onlyOwner {
         if (s_attractions[_name] != address(0)) {
             revert AmusementPark__TheAttractionAlreadyExists();
         }
-        address newAttraction = address(Attraction(_name, _price, _status, address(this)));
+        s_attractions[_name] = Attraction(_name, _price, _status);
         s_attractionNames.push(_name);
-        s_attractions[_name] = newAttraction;
-        emit AttractionAdded(_name, newAttraction);
+        emit AttractionAdded(_name, _price, _status);
     }
 
     function addFood(string memory _name, uint256 _price, bool _status) external onlyOwner {
         if (s_food[_name] != address(0)) {
             revert AmusementPark__TheFoodAlreadyExists();
         }
-        address newFood = address(Food(_name, _price, _status, address(this)));
+        s_food[_name] = Food(_name, _price, _status);
         s_foodNames.push(_name);
-        s_food[_name] = newFood;
-        emit FoodAdded(_name, newFood);
+        emit FoodAdded(_name, _price, _status);
     }
 
     function setAttractionStatus(string memory _name, bool _status) external onlyOwner {
-        if (s_attractions[_name] == address(0)) {
+        if (s_attractions[_name].name == "") {
             revert AmusementPark__TheAttractionDoesNotExist();
         }
-        Attraction tmp = Attraction(s_attractions[_name]);
-        tmp.setStatus(_status);
+        if (s_attractions[_name].status == _status) {
+            revert AmusementPark__AttractionStatusIsAlreadySetted();
+        }
+        s_attractions[_name].status = _status;
+        emit AttractionStatusUpdated(_name, _status);
     }
 
     function setFoodStatus(string memory _name, bool _status) external onlyOwner {
-        if (s_food[_name] == address(0)) {
+        if (s_food[_name] == "") {
             revert AmusementPark__TheFoodDoesNotExist();
         }
-        Food tmp = Food(s_food[_name]);
-        tmp.setStatus(_status);
+        if (s_food[_name].status == _status) {
+            revert AmusementPark__FoodStatusIsAlreadySetted();
+        }
+        s_food[_name].status = _status;
+        emit FoodStatusUpdated(_name, _status);
     }
 
-    function getAttractionsList() external view returns(string[] memory) {
+    function showAttractions() external view returns(string[] memory) {
         return s_attractionNames;
     }
 
-    function getFoodList() external view returns(string[] memory) {
+    function showFoods() external view returns(string[] memory) {
         return s_foodNames;
+    }
+
+    function getOnAttraction(string memory _attractionName) external {
+         if (s_attractions[_attractionName].status == false) {
+             revert AmusementPark__TheAttractionIsNotAvailable();
+         }
+         if (clients[_msgSender()].attractionsPassEndTime < block.timestamp) {
+             uint256 price = s_attractions[_attractionName].price;
+             if (price > myTokens()) {
+                 revert AmusementPark__NotEnoughTokensOnTheClientBalance();
+             }
+             i_token.transferToDisney(_msgSender(), address(this), price);
+         }
+         s_historyAttractions[_msgSender()].push(_attractionName);
+         clients[_msgSender()].attractionsVisited.push(_attractionName);
+         emit ClientVisitedAttraction(_attractionName, price, _msgSender());
+     }
+
+    function buyFood(string memory _foodName) external {
+        if (s_food[_foodName].status == false) {
+             revert AmusementPark__TheFoodIsNotAvailable();
+         }
+         uint256 price = s_food[_foodName].price;
+         if (price > myTokens()) {
+             revert AmusementPark__NotEnoughTokensOnTheClientBalance();
+         }
+         i_token.transferToDisney(_msgSender(), address(this), price);
+         s_historyFood[_msgSender()].push(_foodName);
+         emit ClientBuyedFood(_foodName, price, _msgSender());
+    }
+
+    function showHistoryAttractions() external view returns(string[] memory) {
+        return s_historyAttractions[_msgSender()];
+    }
+
+    function showHistoryFoods() external view returns(string[] memory) {
+        return s_historyFood[_msgSender()];
+    }
+
+    function returnTokens(uint256 _numTokens) external payable {
+        if (_numTokens == 0) {
+            revert AmusementPark__NumTokensIsIncorrect();
+        }
+        if (_numTokens < myTokens()) {
+            revert AmusementPark__NotEnoughTokensOnTheClientBalance();
+        }
+        i_token.transferToDisney(_msgSender(), address(this), _numTokens);
+        payable(_msgSender()).transfer(tokenPrice(_numTokens));
+    }
+
+    function buyPass(PassType _passType) external {
+        uint256 price = s_passSettings[_passType].passPrice;
+        if (price > myTokens()) {
+            revert AmusementPark__NotEnoughTokensOnTheClientBalance();
+        }
+        i_token.transferToDisney(_msgSender(), address(this), price);
+        uint256 currentPassEndTime = clients[_msgSender()].attractionsPassEndTime;
+        if (block.timestamp > currentPassEndTime) {
+            clients[_msgSender()].attractionsPassEndTime = block.timestamp + s_passSettings[_passType].passDuration;
+        } else {
+            clients[_msgSender()].attractionsPassEndTime += s_passSettings[_passType].passDuration;
+        }
+        emit PassBuyed(_msgSender(), _passType);
     }
 }
--- module 6/IDisneyPark.sol
+++ module 6/IDisneyPark.sol
@@ -1,13 +1,116 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.15;
+pragma solidity 0.8.15;
 
-import "./Attraction.sol";
-import "./Food.sol";
 import "./DisneyToken.sol";
 
 contract IDisneyPark {
-    function IDisneyPark(){
 
+    enum PassType {
+       THREE_HOURS,
+       SIX_HOURS,
+       NINE_HOURS,
+       TWELVE_HOURS,
+       DAY
+   }
+
+   struct PassSettings {
+       uint256 passDuration;
+       uint256 passPrice;
+   }
+
+    struct Client {
+        uint256 tokensBuyed;
+        string[] attractionsVisited;
+        uint256 attractionsPassEndTime;
     }
+
+    struct Attraction {
+        string name;
+        uint256 price;
+        bool status;
+    }
+
+    struct Food {
+        string name;
+        uint256 price;
+        bool status;
+    }
+
+    event ClientVisitedAttraction(string indexed name, uint256 price, address indexed walletClient);
+    event ClientBuyedFood(string indexed name, uint256 price, address indexed walletClient);
+    event AttractionAdded(string indexed name, uint256 price, bool status);
+    event AttractionStatusUpdated(string indexed name, bool newStatus);
+    event FoodAdded(string indexed name, uint256 price, bool status);
+    event FoodStatusUpdated(string indexed name, bool newStatus);
+    event PassBuyed(address indexed client, PassType passType);
+
+    // --------------------------------- УПРАВЛЕНИЕ ТОКЕНАМИ ---------------------------------
+
+    // Функция покупки токенов Disney, для использования на атракционах или в ресторане парка
+    // numTokens_: количество токенов, которые клиент хочет купить
+    function buyTokens(uint256 numTokens_) external payable;
+
+    // Баланс токенов контракта Disney
+    function balanceOf() external view returns (uint256);
+
+    // Отображение количества оставшихся токенов клиента
+    function myTokens() external view returns (uint256);
+
+    // Функция для генерации большего количества токенов
+    // numTokens_: сколько нужно сгенерировать новых токенов (Управление только для владельца контракта Disney)
+    function generateNewTokens(uint256 _numTokens) external;
+
+    // --------------------------------- УПРАВЛЕНИЕ ПАРКОМ ---------------------------------
+
+    // Функции только для владельца контракта парка Disney
+    // Создавание нового аттракциона в парке (Управление только для владельца контракта Disney)
+    // attractionName_: название атракциона в парке
+    // price_: цена на билета атракцион
+    function addAttraction(string memory attractionName_, uint256 price_) external;
+
+    // Создавание нового элемента меню в ресторане парка (Управление только для владельца контракта Disney)
+    // foodName_: название элемента питания в ресторане парка
+    // price_: цена элемента питания
+    function addNewFood(string memory foodName_, uint256 price_) external;
+
+    // Изменить статус доступа к атракциону (Управление только для владельца контракта Disney)
+    // attractionName_: название атракциона, к которому должен быть прекращён доступ
+    // status_: новый статус аттракциона
+    function setAttractionStatus(string memory attractionName_, bool status_) external;
+
+    // Изменить статус доступа к определённому элементу меню в ресторане парка (Управление только для владельца контракта Disney)
+    // foodName_: название элемента питания в ресторане парка, который нельзя будет продолжать покупать
+    // status_: новый статус еды
+    function setFoodStatus(string memory foodName_, bool _status) external;
+
+    // Функции для клиентов парка Disney
+    // Посмотреть список всех атракционов в парке Disney
+    function showAttractions() external view returns (string[] memory);
+
+    // Посмотреть весь список элементов еды (продуктов) в ресторане парка Disney
+    function showFoods() external view returns (string[] memory);
+
+    // Функция, чтобы попасть на аттракцион парка Disney и заплатить токенами
+    // attractionName_: название атракциона, на котором хочет прокатиться клиент
+    function getOnAttraction(string memory attractionName_) external;
+
+    // Функция покупки еды за токены
+    // foodName_: название элемента питания, который хотел бы купить клиент
+    function buyFood(string memory foodName_) external;
+
+    // Просмотр полной истории аттракционов, которые посетил клиент (клиент может узнать только свою историю посещения атракционов)
+    function showHistoryAttractions() external view returns (string[] memory);
+
+    // Просмотр полной истории элементов еды в меню, которые покупал клиент (клиент может узнать только свою историю покупок продуктов)
+    function showHistoryFoods() external view returns (string[] memory);
+
+    // Функция, позволяющая клиенту Disney возвращать токены парку в обмен на нативную валюту блокчейна
+    // numTokens_: количество возвращаемых токенов в обмен на нативную монету сети
+    function returnTokens(uint256 numTokens_) external payable;
+
+    // Функция, позволяющая клиенту Disney покупать абонемент
+    // passType_: тип абонемента
+    function buyPass(PassType passType_) external;
+
 }
